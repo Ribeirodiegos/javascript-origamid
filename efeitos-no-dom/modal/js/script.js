94b2d4e5e0808cae62bcc2743863b3851c44d64f
Ribeirodiegos <santos.diegoribeiro@gmail.com>
2024-03-06 23:02:07 -0300

    Exercicios - Modal

--- efeitos-no-dom/modal/js/script.js
+++ efeitos-no-dom/modal/js/script.js
@@ -2,8 +2,10 @@ import initTabNav from "./modules/tabnav.js";
 import initAccordion from "./modules/accordion.js";
 import initScrollSuave from "./modules/scroll-suave.js";
 import initAnimacaoScroll from "./modules/anima-scroll.js";
+import initModal from "./modules/modal.js";
 
 initTabNav()
 initAccordion();
 initScrollSuave();
 initAnimacaoScroll();
+initModal();
