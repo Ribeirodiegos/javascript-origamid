34f818a4610d42dffbec48ad48040a210046d5f4
Ribeirodiegos <santos.diegoribeiro@gmail.com>
2024-03-14 23:50:22 -0300

    Aula - começo

---
+++ efeitos-no-dom/tooltip/js/script.js
@@ -0,0 +1,15 @@
+import initTabNav from "./modules/tabnav.js";
+import initAccordion from "./modules/accordion.js";
+import initScrollSuave from "./modules/scroll-suave.js";
+import initAnimacaoScroll from "./modules/anima-scroll.js";
+import initModal from "./modules/modal.js";
+import initTooltip from "./modules/tooltip.js";
+
+
+initTabNav()
+initAccordion();
+initScrollSuave();
+initAnimacaoScroll();
+initModal();
+initTooltip();
+
